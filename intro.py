3361a4b5a9b8469ea548bdb8f01e22c5c8743594
SaraHM0330 <160879031+SaraHM0330@users.noreply.github.com>
2024-05-21 15:34:17 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
 st.title("Página de Inicio")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend.")
+image=Image.open("CasaObraBlanca.jpg")
+st.image(image, caption= "Esta es la casa")
+
+
+st.subheader("Ahora Usemos 2 columnas")
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp=st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("correcto") 
+with col2:
+  st.subheader("Esta es la segunda")
+  modo=st.radio("Que modalidad es la principal de tu interfaz",("Visual", "auditivo", "tactil"))
+  if modo=="Visual":
+    st.write("lA VISTA ES FUNDAMENTAL PARA TU INTERFAZ")
+  if modo=="auditivo":
+    st.write("skjasjdklas")
+  if modo=="tactil":
+    st.write("jdasdlaksdadoj")
