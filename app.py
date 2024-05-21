40450a41a2c89aa16f93aa224faae56035227a6e
SaraHM0330 <160879031+SaraHM0330@users.noreply.github.com>
2024-05-21 17:25:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,11 +54,11 @@ if img_file_buffer is not None:
     # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][1]>0.3:
-      st.header('Cerrando')
-      client1.publish("accion","{'gesto': 'Cerrando'}",qos=0, retain=False)
-      time.sleep(0.2)
     if prediction[0][0]>0.3:
+      st.header('Cerrando')
+      client1.publish("accion","{'gesto': 'Cierra'}",qos=0, retain=False)
+      time.sleep(0.2)
+    if prediction[0][1]>0.3:
       st.header('Abriendo')
-      client1.publish("accion","{'gesto': 'Abrir'}",qos=0, retain=False)
+      client1.publish("accion","{'gesto': 'Abre'}",qos=0, retain=False)
       time.sleep(0.2)  
